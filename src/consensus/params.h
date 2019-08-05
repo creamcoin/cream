b8a7503f490ebe85ba935658a83fe49ad390c8eb
Peter Bushnell <bushsolo@gmail.com>
2019-08-05 10:25:57 +0100

    Chainparams

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -49,11 +49,8 @@ struct BIP9Deployment {
 struct Params {
     uint256 hashGenesisBlock;
     int nSubsidyHalvingInterval;
-    /* Block hash that is excepted from BIP16 enforcement */
-    uint256 BIP16Exception;
     /** Block height and hash at which BIP34 becomes active */
     int BIP34Height;
-    uint256 BIP34Hash;
     /** Block height at which BIP65 becomes active */
     int BIP65Height;
     /** Block height at which BIP66 becomes active */
